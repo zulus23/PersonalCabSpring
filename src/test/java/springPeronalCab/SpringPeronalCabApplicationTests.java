ccf219326f3cd8efb5892fce60eac09a828680a2
zulus23 <zulus23@gmail.com>
2015-08-27 16:02:25 +0300

    Setup project

---
+++ src/test/java/springPeronalCab/SpringPeronalCabApplicationTests.java
@@ -0,0 +1,18 @@
+package springPeronalCab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringPeronalCabApplication.class)
+@WebAppConfiguration
+public class SpringPeronalCabApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
